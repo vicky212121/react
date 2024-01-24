0e2ceb8f253b7996e13c035a00097299450a6985
vicky212121 <vignesh2105007@gmail.com>
2024-01-25 01:25:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header.jsx'
+import Notes from './components/Notes.jsx'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <Header />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
